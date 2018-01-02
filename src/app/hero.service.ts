cc744ebdc56dd3ccd5e6752d25b581354d0c1529
Wataru.Kasahara <kasahara1019@gmail.com>
2018-01-02 16:27:39 +0900

    HeroService に MessageService を注入

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,10 +5,12 @@ import { of } from 'rxjs/observable/of';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
+import { MessageService } from './message.service';
+
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     return of(HEROES);
