369c10fa5af0f7c888ffc2fcd915bbe4428b2788
Wataru.Kasahara <kasahara1019@gmail.com>
2018-01-05 12:26:50 +0900

    messageService のラッパーメソッドを作成

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,13 +13,17 @@ export class HeroService {
 
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
+    this.log('fetched heroes');
     return of(HEROES);
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
 }
